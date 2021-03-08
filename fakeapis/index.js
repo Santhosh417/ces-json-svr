3c1dd095aaab00d43213bed845dec71e36c55e7d
ShravyaChandiri <schandiri@unomaha.edu>
2021-03-07 18:17:28 -0600

    Updates JSON objects
    Updates JSON objects

--- fakeapis/index.js
+++ fakeapis/index.js
@@ -1,10 +1,12 @@
 const courses = require('./course.json');
 const students = require('./student.json');
 const enrollments = require('./enrollment.json');
+const users = require('./users.json');
 // Something more
 
 module.exports = () => ({
   courses: courses.courses,
   students: students.students,
-  enrollments: enrollments.enrollments
+  enrollments: enrollments.enrollments,
+  users:users.users
 });
